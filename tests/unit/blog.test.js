40fe74d87c8d48e1be8bb3b12deee89de4355038
Tianyu <1374736640@qq.com>
2022-05-21 12:35:03 +0800

    Test(add) test getMetaInfo

--- tests/unit/blog.test.js
+++ tests/unit/blog.test.js
@@ -1,7 +1,26 @@
-const {getPreviousNextIds} = require('../../lib/blog.ts')
+const {getPreviousNextIds, getMetaInfo} = require('../../lib/blog.ts')
 
-test('得到前一页和后一页', () => {
+test('通过id得到前一页和后一页', () => {
     const { nextId, previousId } = getPreviousNextIds('02-install')
-    expect(nextId).toEqual('03-md-example')
-    expect(previousId).toEqual('01-first-post')
+    expect(nextId.id).toEqual('03-md-example')
+    expect(previousId.id).toEqual('01-first-post')
+})
+
+test('测试边界情况，第一页的id的前一页为空', () => {
+    const { nextId, previousId } = getPreviousNextIds('01-first-post')
+    expect(nextId.id).toEqual('02-install')
+    expect(previousId.id).toEqual('')
+})
+
+
+test('得到该id的metaInfo', () => {
+    const { title, description } = getMetaInfo('02-install')
+    expect(title).toEqual('How To Set Up NextPortfolio')
+    expect(description).toEqual('A tutorial about building your website with Next-Portfolio')
+})
+
+test('空id的metaInfo也为空', () => {
+    const { id, description } = getMetaInfo('')
+    expect(id).toEqual('')
+    expect(description).toEqual(undefined)
 })
